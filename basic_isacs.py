3beb6b0da92648cf3d1f5e53bd11fda3f5077040
Vasiliy <vasiliy.trunov.00@mail.ru>
2022-08-04 23:17:00 +0200

    добавил команды изменить и удалить, пока что они не работают

--- basic_isacs.py
+++ basic_isacs.py
@@ -1,4 +1,3 @@
-from operator import length_hint
 from openpyxl import Workbook
 from openpyxl import load_workbook
 
@@ -9,6 +8,7 @@ l1 = (lib['l1']) #переменная с листом с именем 'l1'
 class metal (object):
     counter = 2 #это счетчик строк
                 #начинается с 2, потому что 1 строка будет занята заголовками
+    position_number = 1 #номер позиции у детали для ее вызова
     metal_list = [] #массив для создания имен профилей
     
     @staticmethod
@@ -28,7 +28,7 @@ class metal (object):
                 return send
 
     @staticmethod
-    def length_check(string_1):
+    def length_check(string_1): #проверка введенного количества на наличие лишних символов
         sent_string = string_1
         if sent_string == 'отмена':
             return False
@@ -47,7 +47,8 @@ class metal (object):
         if verification == False:
             self.__del__()
         else:
-            self.profile = verification[0]                    #обязательным аргументом для создания класса является номер профиля 
+            self.profile = verification[0]                    #обязательным аргументом для создания класса является номер профиля
+            print('Добавлен профиль - {}'.format(verification[0])) 
             self.unit_weight = verification[1]                      #задается после создания экземпляра
             length = metal.length_check(input('Введите длину детали: '))                           #задается после создания экземпляра
             if length == False: 
@@ -55,6 +56,8 @@ class metal (object):
             else:
                 self.length = length
                 self.counter_number = metal.counter       #каждому экземпляру задается номер строки
+                self.position_number = metal.position_number    #каждому экземпляру задается свой номер
+                metal.position_number+=1
                 metal.counter+=1
     
     def __del__(self):
@@ -73,16 +76,19 @@ def file_creation(): #функция для создания файла
     if title_name == '': exel_file_1.title = 'basic' #в файле будет называется basic если ничего не введено
     else: exel_file_1.title = '{}'.format(title_name)
 
-    exel_file_1['A1'] = 'Материал'
-    exel_file_1['B1'] = 'Вес 1 м.п.'
-    exel_file_1['C1'] = 'Количество м.п.'
-    exel_file_1['D1'] = 'Масса'
+    exel_file_1['A1'] = '№ п.п.'
+    exel_file_1['B1'] = 'Материал'
+    exel_file_1['C1'] = 'Вес 1 м.п.'
+    exel_file_1['D1'] = 'Количество м.п.'
+    exel_file_1['E1'] = 'Масса'
+    
     # тело для записи данных в файл
     for i in metal.metal_list:
-        exel_file_1['A{}'.format(i.counter_number)] = i.profile
-        exel_file_1['B{}'.format(i.counter_number)] = i.unit_weight
-        exel_file_1['C{}'.format(i.counter_number)] = i.length
-        exel_file_1['D{}'.format(i.counter_number)] = '=C{}*B{}'.format(i.counter_number, i.counter_number)
+        exel_file_1['A{}'.format(i.counter_number)] = i.position_number
+        exel_file_1['B{}'.format(i.counter_number)] = i.profile
+        exel_file_1['C{}'.format(i.counter_number)] = i.unit_weight
+        exel_file_1['D{}'.format(i.counter_number)] = i.length
+        exel_file_1['E{}'.format(i.counter_number)] = '=C{}*D{}'.format(i.counter_number, i.counter_number)
 
     #конец записи и сохранение файла
     print('Конец записи')
@@ -90,11 +96,16 @@ def file_creation(): #функция для создания файла
 
 
 while True:
+    print('--------------------------------------------------')
     print('0 - Выход')
     print('1 - Узнать вес нужного количеста заданного номера')
-    print('узнать о наличии нужного сечения в библиотеке')
-    print('узнать длину контура данной детали')
-    input_a = int(input('введите номер операции - '))   #ДОБАВИТЬ ОБРАБОТКУ ИСКЛЮЧЕНИЙ 
+    print('2 - Узнать длину контура профиля')
+    try:
+        input_a = int(input('введите номер операции - '))   #ДОБАВИТЬ ОБРАБОТКУ ИСКЛЮЧЕНИЙ
+    except ValueError:
+        print('--------------------------------------------------')
+        print('Введен некорректный номер операции - {}'.format(input_a)) 
+        continue
     if input_a == 0: 
         print('Выход')
         break
@@ -104,12 +115,38 @@ while True:
             if input_b.lower() == 'конец': break
             elif input_b.lower() == 'показ':
                 for i in metal.metal_list:
-                    print('№ {} - {} м.п. весит - {} кг'.format(i.profile, i.length, i.unit_weight))
+                    print('№{} {} - {} м.п., 1 м весит - {} кг, общая масса - {}'.format(i.position_number ,i.profile, i.length, i.unit_weight, i.unit_weight*i.length))
                 continue
+            elif input_b.lower() == 'изменить':
+                print('Введите "удалить", чтобы удалить номер профиля из списка')
+                print('Введите "изменить", чтобы изменить количество материала (длину)')
+                input_c = input('Введите действие - ').lower()
+                if input_c == 'удалить':
+                    input_d = input('Введите номер позиции удаляемого элемента - ')
+                    for n, i in enumerate(metal.metal_list):
+                        if i.position_number == input_c:
+                            del(i) #Добавить функцию для добавления или изменения
+                            print('Выполнено')
+                    continue
+                if input_c == 'изменить':
+                    input_d = input('Введите номер позиции изменяемого элемента - ').upper()
+                    for i in metal.metal_list:
+                        if i.position_number == input_d:
+                            input_e = input('Введите новое количество - ')
+                            if metal.length_check(input_e) != False: i.length = float(input_e)
+                            print('Изменения внесены')
+                            continue
+                if input_c == 'отмена':
+                    print('Отмена действия')
+                    continue
             elif input_b.lower() == 'запись':
                 file_creation()
                 break
             check = metal(input_b.upper())
             metal.metal_list.append(check)
+    
+    if input_a == 2: #после заполнения библиотеки
+        pass
+
         
         
